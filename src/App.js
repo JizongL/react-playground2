b0d8f2e3d82822f7475ac57e23fb5609499ebf37
JizongL <2015rpro@gmail.com>
2019-03-18 23:03:26 -0400

    finish smoke test and snapshot test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css'
 import Split from './composition/Split'
+import Messages from './Messages'
+
 function App() {
   return (
-    <main className='App'>
-      <Split className = 'left'>
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
-      </Split>
-      <Split className = 'right'flexBasis={2}>
-        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
+    < div className='App'>
+    <h1>YOUR APPLICATION NAME!</h1>
+      <Messages name='Messages' unread={0}/>
+      <Messages name='Notification' unread={10}/>
+    </div>
   )
 }
 
